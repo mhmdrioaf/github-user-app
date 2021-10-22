b3198fab4525399f9e2aafb58d94b88daa9f3865
Rio Ananta <rioananta1@gmail.com>
2021-10-22 18:35:37 +0700

    Remove Headers Token

--- app/src/main/java/com/ananta/githubusersapp/ApiService.kt
+++ app/src/main/java/com/ananta/githubusersapp/ApiService.kt
@@ -5,25 +5,21 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
     fun getUser(
         @Query("q") login: String
     ): Call<UserSearchResponse>
 
     @GET("users/{login}")
-    @Headers("Authorization: token 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")
     fun getUserDetail(
         @Path("login") login: String
     ): Call<UserResponse>
 
     @GET("users/{login}/followers")
-    @Headers("Authorization: token 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")
     fun getUserFollowers(
         @Path("login") login: String
     ): Call<List<PersonItem>>
 
     @GET("users/{login}/following")
-    @Headers("Authorization: token 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")
     fun getUserFollowing(
         @Path("login") login: String
     ): Call<List<PersonItem>>
